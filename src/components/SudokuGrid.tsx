d45c41bf301b2e8cfd15421ca9b3e1d53a42cafd
NoxyMe <Reddashgames@gmail.com>
2024-03-06 21:54:51 +0000

    Quick clean up pass

--- src/components/SudokuGrid.tsx
+++ src/components/SudokuGrid.tsx
@@ -3,19 +3,21 @@
 import React, { useEffect, useState } from 'react';
 import styles from "./SudokuGrid.module.scss";
 
-/*
-// 6x6
-const sub_grid_w   = 3;
-const sub_grid_h   = 2;
-const total_grid_w = 6;
-const total_grid_h = 6;
-*/
+// Define your subgrid and total grid dimensions here (Will be replaced by a simple drop down box, mostly here for testing.)
 /*
 // 4x4
 const sub_grid_w   = 2;
 const sub_grid_h   = 2;
 const total_grid_w = 4;
 const total_grid_h = 4;
+*/
+
+/*
+// 6x6
+const sub_grid_w   = 3;
+const sub_grid_h   = 2;
+const total_grid_w = 6;
+const total_grid_h = 6;
 */
 
 // classic 9x9
@@ -25,6 +27,13 @@ const total_grid_w = 9;
 const total_grid_h = 9;
 
 /*
+// classic 12x12  [Will not work as we are limited with inputs right now from 1-9]
+const sub_grid_w   = 4;
+const sub_grid_h   = 3;
+const total_grid_w = 12;
+const total_grid_h = 12;
+*/
+/*
 // 16x16 [Will not work as we are limited with inputs right now from 1-9]
 const sub_grid_w   = 4;
 const sub_grid_h   = 4;
@@ -32,13 +41,23 @@ const total_grid_w = 16;
 const total_grid_h = 16;
 */
 
+
+interface Cell {
+    value: number;
+    locked: boolean;
+}
+
 interface SudokuCellProps {
     row: number;
     column: number;
     isSelected: boolean;
     isSelectionAligned: boolean;
-    value?: number;
+    value: number;
+    isLocked: boolean;
     onCellClick: (row: number, column: number) => void;
+    isLineHor: boolean;
+    isLineVer: boolean;
+    isValueShown: boolean;
 }
 
 export const SudokuCell: React.FC<SudokuCellProps> = ({
@@ -66,15 +85,26 @@ export const SudokuCell: React.FC<SudokuCellProps> = ({
         <button 
             className={classes.join(' ')} 
             onClick={() => onCellClick(row, column)}
+            disabled={isLocked}
         >
             {value ? value : ''}
         </button>
     );
 };
 
-
-
-export function SudokuRow({row, cells, selectedRow, selectedColumn, onCellClick}) {
+export function SudokuRow({
+    row,
+    cells,
+    selectedRow,
+    selectedColumn,
+    onCellClick
+}: {
+    row: number;
+    cells: Cell[];
+    selectedRow: number;
+    selectedColumn: number;
+    onCellClick: (row: number, column: number) => void;
+}) {
     return <div className={styles.row}>{
         cells.map((cell, col) => (
             <SudokuCell
@@ -83,15 +113,15 @@ export function SudokuRow({row, cells, selectedRow, selectedColumn, onCellClick}
                 column={col}
                 value={cell.value}
                 isLocked={cell.locked}
-                isSelected={selectedColumn == col && selectedRow == row}
+                isSelected={selectedColumn === col && selectedRow === row}
                 isSelectionAligned={
-                    selectedRow == row || selectedColumn == col ||
-                    (Math.floor(selectedColumn/sub_grid_w) == Math.floor(col/sub_grid_w) &&
-                     Math.floor(selectedRow/sub_grid_h) == Math.floor(row/sub_grid_h))
+                    selectedRow === row || selectedColumn === col ||
+                    (Math.floor(selectedColumn / sub_grid_w) === Math.floor(col / sub_grid_w) &&
+                     Math.floor(selectedRow / sub_grid_h) === Math.floor(row / sub_grid_h))
                 }
-                isLineHor={col % sub_grid_w == sub_grid_w-1 && col != total_grid_w-1}
-                isLineVer={row % sub_grid_h == sub_grid_h-1 && row != total_grid_h-1}
-                isValueShown={cell.value != 0}
+                isLineHor={col % sub_grid_w === sub_grid_w - 1 && col !== total_grid_w - 1}
+                isLineVer={row % sub_grid_h === sub_grid_h - 1 && row !== total_grid_h - 1}
+                isValueShown={cell.value !== 0}
                 onCellClick={onCellClick}
             />
         ))}
@@ -99,63 +129,58 @@ export function SudokuRow({row, cells, selectedRow, selectedColumn, onCellClick}
 }
 
 export default function SudokuGrid() {
-	const emptyGrid = Array(total_grid_h).fill(null).map(() =>
-		Array(total_grid_w).fill(null).map(() => ({ value: 0, locked: false }))
-	);
-	
-    const [grid, setGrid] = useState(emptyGrid); // Copy initial grid into state
+    const emptyGrid: Cell[][] = Array(total_grid_h).fill(null).map(() =>
+        Array(total_grid_w).fill(null).map(() => ({ value: 0, locked: false }))
+    );
+    
+    const [grid, setGrid] = useState(emptyGrid);
     const [selectedRow, setSelectedRow] = useState(0);
     const [selectedColumn, setSelectedColumn] = useState(0);
 
-    const handleCellClick = (row, col) => {
+    const handleCellClick = (row: number, col: number) => {
         setSelectedRow(row);
         setSelectedColumn(col);
     };
 
-    // Incorporate SetCell logic directly here
-	const setCell = (row, col, num, locked) => {
-		const newGrid = [...grid]; // Clone the grid for immutability
-		//if (newGrid[row] && !newGrid[row][col].locked) { // Check if the cell exists is not locked
-		if (newGrid[row] ) { // Check if the cell exists is not locked
-			const updatedCell = { ...newGrid[row][col], value: num, locked: num != 0 && locked }; // Update the cell
-			newGrid[row] = [
-				...newGrid[row].slice(0, col),
-				updatedCell,
-				...newGrid[row].slice(col + 1)
-			];
-			setGrid(newGrid);
-		}
-	};
+    const setCell = (row: number, col: number, num: number, locked: boolean) => {
+        const newGrid = [...grid];
+        if (newGrid[row]) {
+            const updatedCell = { ...newGrid[row][col], value: num, locked: (num !== 0) && locked };
+            newGrid[row] = [
+                ...newGrid[row].slice(0, col),
+                updatedCell,
+                ...newGrid[row].slice(col + 1)
+            ];
+            setGrid(newGrid);
+        }
+    };
 
-
-    const handleKeyDown = (event) => {
-		console.log(event.key)
+    const handleKeyDown = (event: KeyboardEvent) => {
         switch (event.key) {
             case 'ArrowUp':
-                setSelectedRow((prevRow) => Math.max(0, prevRow - 1));
+                setSelectedRow(prevRow => Math.max(0, prevRow - 1));
                 break;
             case 'ArrowDown':
-                setSelectedRow((prevRow) => Math.min(grid.length - 1, prevRow + 1));
+                setSelectedRow(prevRow => Math.min(grid.length - 1, prevRow + 1));
                 break;
             case 'ArrowLeft':
-                setSelectedColumn((prevCol) => Math.max(0, prevCol - 1));
+                setSelectedColumn(prevCol => Math.max(0, prevCol - 1));
                 break;
             case 'ArrowRight':
-                setSelectedColumn((prevCol) => Math.min(grid[0].length - 1, prevCol + 1));
+                setSelectedColumn(prevCol => Math.min(grid[0].length - 1, prevCol + 1));
                 break;
-            case 'Backspace':// clear cell
+            case 'Backspace': // Clear cell
             case 'Delete':
-                setCell(selectedRow, selectedColumn, 0, false); // not working
+                setCell(selectedRow, selectedColumn, 0, false);
                 break;
-            default: // add number to cell
-                if (event.key >= '0' && event.key <= '9') {
+            default: // Add number to cell
+                if (event.key >= '1' && event.key <= '9') {
                     setCell(selectedRow, selectedColumn, parseInt(event.key), true);
                 }
                 break;
         }
     };
 
-    // Effect to add and remove the event listener
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
         return () => {
@@ -164,7 +189,7 @@ export default function SudokuGrid() {
     }, [selectedRow, selectedColumn, grid]); // Depend on selectedRow, selectedColumn, and grid so the effect updates with state
 
     return <div className={styles.grid} tabIndex={0}>
-        {grid.map((cells, index) =>
+        {grid.map((cells, index) => (
             <SudokuRow
                 key={index}
                 row={index}
@@ -172,6 +197,8 @@ export default function SudokuGrid() {
                 selectedRow={selectedRow}
                 selectedColumn={selectedColumn}
                 onCellClick={handleCellClick}
-            />)}
+            />
+        ))}
     </div>;
 }
+
